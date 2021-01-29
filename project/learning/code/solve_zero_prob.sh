6d4daf606e5e1658766bc84a2965d3d751b138b1
Bennouhan <bjn20@ic.ac.uk>
2021-01-29 18:12:49 +0000

    solve zero problem

---
+++ project/learning/code/solve_zero_prob.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Ben Nouhan, bjn20@ucl.ac.uk
+# Desc: Script that is run to solve the zero issue with tensorflow
+
+for a in /sys/bus/pci/devices/*; do echo 0 | sudo tee -a $a/numa_node; done
+
+#link to line of code: https://stackoverflow.com/questions/44232898/memoryerror-in-tensorflow-and-successful-numa-node-read-from-sysfs-had-negativ
+#link to explanation of how to run automatically at startup: https://askubuntu.com/questions/814/how-to-run-scripts-on-start-up
+#    (answer by ceejayoz. used nano.)
+#    crontab -e
+#    then @reboot bash [absolute path to sh script you want to run at reboot, /home/bennouhan/and so on]
